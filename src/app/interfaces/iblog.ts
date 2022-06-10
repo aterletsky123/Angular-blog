83f3d3b1daf33821c847d43ecc15dde17ba47460
aterletsky123 <terletsky28@gmail.com>
2022-06-10 18:46:58 +0300

    create modal windows

---
+++ src/app/interfaces/iblog.ts
@@ -0,0 +1,7 @@
+export interface IBlog {
+    id: number | string,
+    postedBy: string,
+    topic: string,
+    date: string,
+    message: string
+}
---
+++ src/app/interfaces/iusers.ts
@@ -0,0 +1,6 @@
+export interface IUsers {
+    id: number | string,
+    username: string,
+    email: string,
+    password: string
+}
